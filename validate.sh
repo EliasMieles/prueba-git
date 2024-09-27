c32325372671acdd7395c6fe5f1c237d2fad9e48
Mieles <eliasmieles0325@gmail.com>
2024-09-27 00:13:42 -0500

    Función validate trabajada en la rama master

---
+++ validate.sh
@@ -0,0 +1,2 @@
+# !/bin/bash
+echo "Trabajando en la funcionalidad de validar en la rama main, archivo generado como ejemplo de como sería el orden o las funcionalidades si se trabaja en dos ramas distintas"
