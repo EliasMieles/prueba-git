b28a678a7303cf2931541c620fb12ed16f9784f7
Mieles <eliasmieles0325@gmail.com>
2024-09-27 00:31:50 -0500

    Actualizando la información del archivo example_git.sh

--- example_git.sh
+++ example_git.sh
@@ -1,2 +1,3 @@
 # !/bin/bash
-echo "NEW Hola mundo, control de versiones con git"	
+echo "NEW Hola mundo, control de versiones con git"
+echo "TRABAJANDO EN MAIN(ACTUALIZAR LOS DE LOGIN, POR FAVOR)"	
